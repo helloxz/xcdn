dce3d4d947da43c065d479f08076c715cf610086
xiaoz <xiaoz93@outlook.com>
2021-11-17 21:25:52 +0800

    fix bug

--- sh/xcdn.sh
+++ sh/xcdn.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 ############### XCDN管理脚本 ###############
-#Author:xiaoz.me
-#Update:2021-11-17
-#Github:https://github.com/helloxz/xcdn
+# Author:xiaoz.me
+# Update:2021-11-17
+# Github:https://github.com/helloxz/xcdn
 ####################### END #######################
 
 #nginx路径
 NGINX_PATH="/usr/local/nginx"
 nginx="${NGINX_PATH}/sbin/nginx"
+
+#获取用户传递的参数
+arg1=$1
 
 #启动脚本
 function start(){
@@ -27,7 +30,34 @@ function quit() {
     $nginx -c /data/xcdn/conf/nginx.conf -s quit
 }
 
-#重载脚本
+#重载配置
 function reload(){
 	$nginx -c /data/xcdn/conf/nginx.conf -t && $nginx -c /data/xcdn/conf/nginx.conf -s reload
 }
+
+# 检查配置
+function check_conf() {
+    $nginx -c /data/xcdn/conf/nginx.conf -t
+}
+
+# 根据用户输入执行不同动作
+case $VAR in
+    start) 
+        start
+    ;;
+    stop) 
+        stop
+    ;;
+    quit)
+        quit
+    ;;
+    reload)
+        reload
+    ;;
+    -t)
+        check_conf
+    ;;
+    *) 
+        echo 'Parameter error!'
+    ;;
+esac
